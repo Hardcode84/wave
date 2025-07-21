e846952c249e49dfa5b9217220447a82d66803d4
Ivan Butygin <ivan.butygin@gmail.com>
2025-07-21 09:17:25 +0200

    cleanup
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- wave_lang/kernel/wave/codegen/emitter.py
+++ wave_lang/kernel/wave/codegen/emitter.py
@@ -4,7 +4,6 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from math import prod
 import sys
 from itertools import chain
 from collections import namedtuple
