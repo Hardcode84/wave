aeb23f686fec6399f103a8aeebfda69980a5f4f6
Ivan Butygin <ivan.butygin@gmail.com>
2025-09-29 16:31:42 +0200

    hipinit
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- tests/kernel/wave/wave_e2e_test.py
+++ tests/kernel/wave/wave_e2e_test.py
@@ -4,6 +4,9 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from hip import hip
+hip.hipInit(0)
+
 import json
 import os
 
